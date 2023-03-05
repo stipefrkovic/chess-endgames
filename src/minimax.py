46770f35e84b79ac2c83d9e44c610ae3a25c8148
stipefrkovic <stipe.frkovic@tutanota.com>
2023-03-05 18:51:21 +0100

    minimax working

--- src/minimax.py
+++ src/minimax.py
@@ -10,10 +10,13 @@ class Variation:
         self.moves.append(move)
 
 
+# TODO implement alpha beta pruning
+# TODO prefer shorter variations
 def td_minimax(board, turn, depth, max_depth):
     outcome = board.outcome()
     if depth is max_depth or outcome is not None:
         if depth is max_depth:
+            # TODO add real evaluation
             evaluation = 0
         elif outcome.winner is None:
             evaluation = 0
@@ -27,7 +30,7 @@ def td_minimax(board, turn, depth, max_depth):
         return variation
 
     variations = []
-    for move in board.legal_moves():
+    for move in board.legal_moves:
         board.push(move)
         variation = td_minimax(board, board.turn, depth+1, max_depth)
         variations.append(variation)
@@ -40,3 +43,18 @@ def td_minimax(board, turn, depth, max_depth):
         raise Exception("Error with turn.")
     principal_variation.add_move(board.fen())
     return principal_variation
+
+
+def main():
+    mate_in_1 = "8/8/8/k1K5/8/1R6/8/8 w - - 0 1"
+    mate_in_2 = "k7/8/2K5/8/8/1R6/8/8 w - - 0 1"
+    mate_in_3 = "8/k7/8/2K5/8/1R6/8/8 w - - 0 1"
+    board = chess.Board(mate_in_2)
+    principal_variation = td_minimax(board, chess.WHITE, 0, 4)
+    print(principal_variation.evaluation)
+    for move in principal_variation.moves:
+        print(chess.Board(move))
+
+
+if __name__ == "__main__":
+    main()
