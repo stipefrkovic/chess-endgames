29a93e06ceaa2fdb5d8aae4c4433866b3026454f
stipefrkovic <stipe.frkovic@tutanota.com>
2023-03-06 17:15:14 +0100

    alpha-beta pruning, mlp changes, deleted unused files

--- src/minimax.py
+++ src/minimax.py
@@ -1,3 +1,5 @@
+import time
+
 import chess
 
 
@@ -10,13 +12,10 @@ class Variation:
         self.moves.append(move)
 
 
-# TODO implement alpha beta pruning
-# TODO prefer shorter variations
-def td_minimax(board, turn, depth, max_depth):
+def minimax(board, depth, max_depth, turn):
     outcome = board.outcome()
     if depth is max_depth or outcome is not None:
         if depth is max_depth:
-            # TODO add real evaluation
             evaluation = 0
         elif outcome.winner is None:
             evaluation = 0
@@ -32,7 +31,7 @@ def td_minimax(board, turn, depth, max_depth):
     variations = []
     for move in board.legal_moves:
         board.push(move)
-        variation = td_minimax(board, board.turn, depth+1, max_depth)
+        variation = minimax(board, depth + 1, max_depth, board.turn)
         variations.append(variation)
         board.pop()
     if turn is chess.WHITE:
@@ -45,15 +44,69 @@ def td_minimax(board, turn, depth, max_depth):
     return principal_variation
 
 
+# TODO can this be extended such that if alpha or beta are best the search can stop?
+# TODO prefer shorter variations?
+def alphabeta(board, depth, max_depth, turn, alpha, beta):
+    outcome = board.outcome()
+    if depth is max_depth or outcome is not None:
+        if depth is max_depth:
+            evaluation = 0
+            # TODO add real evaluation
+        elif outcome.winner is None:
+            evaluation = 0
+        elif outcome.winner is chess.WHITE:
+            evaluation = 1
+        elif outcome.winner is chess.BLACK:
+            evaluation = -1
+        else:
+            raise Exception("Error with outcome.")
+        variation = Variation(evaluation, board.fen())
+        return variation
+
+    if turn is chess.WHITE:
+        principal_variation = Variation(-100, None)
+        for move in board.legal_moves:
+            board.push(move)
+            variation = alphabeta(board, depth + 1, max_depth, board.turn, alpha, beta)
+            principal_variation = max([principal_variation, variation], key=lambda x: x.evaluation)
+            board.pop()
+            if principal_variation.evaluation > beta:
+                break
+            alpha = max(alpha, principal_variation.evaluation)
+    elif turn is chess.BLACK:
+        principal_variation = Variation(100, None)
+        for move in board.legal_moves:
+            board.push(move)
+            variation = alphabeta(board, depth + 1, max_depth, board.turn, alpha, beta)
+            principal_variation = min([principal_variation, variation], key=lambda x: x.evaluation)
+            board.pop()
+            if principal_variation.evaluation < alpha:
+                break
+            beta = min(beta, principal_variation.evaluation)
+    else:
+        raise Exception("Error with turn.")
+    principal_variation.add_move(board.fen())
+    return principal_variation
+
+
 def main():
     mate_in_1 = "8/8/8/k1K5/8/1R6/8/8 w - - 0 1"
     mate_in_2 = "k7/8/2K5/8/8/1R6/8/8 w - - 0 1"
     mate_in_3 = "8/k7/8/2K5/8/1R6/8/8 w - - 0 1"
+
     board = chess.Board(mate_in_2)
-    principal_variation = td_minimax(board, chess.WHITE, 0, 4)
-    print(principal_variation.evaluation)
-    for move in principal_variation.moves:
-        print(chess.Board(move))
+    start_time = time.time()
+    principal_variation = minimax(board, 0, 5, chess.WHITE)
+    print("Minimax - Time: %.4s, Evaluation: %s" % (time.time() - start_time, principal_variation.evaluation))
+
+    board = chess.Board(mate_in_2)
+    start_time = time.time()
+    principal_variation = alphabeta(board, 0, 5, chess.WHITE, -100, 100)
+    print("Alphabeta - Time: %.4s, Evaluation: %s" % (time.time() - start_time, principal_variation.evaluation))
+
+    # print(principal_variation.evaluation)
+    # for move in principal_variation.moves:
+    #     print(chess.Board(move))
 
 
 if __name__ == "__main__":
--- src/mlp.py
+++ src/mlp.py
@@ -1,10 +1,12 @@
+import os
+
 import tensorflow as tf
 
 
 class MLP:
-    def __init__(self):
+    def __init__(self, model_weights_path='weights/weights.h5'):
+        self.model_weights_path = model_weights_path
         self.model = None
-        self.model_checkpoint_dir = '/model/checkpoint'
 
     def build(self):
         self.model = tf.keras.Sequential([
@@ -12,28 +14,33 @@ class MLP:
             tf.keras.layers.Dense(64, activation=tf.keras.activations.relu),
             tf.keras.layers.Dense(1, activation=tf.keras.activations.sigmoid),
         ])
+
+    def load_weights(self):
+        if os.path.isfile(self.model_weights_path):
+            self.model.load_weights(self.model_weights_path)
+
+    def compile(self):
         self.model.compile(optimizer=tf.keras.optimizers.Adam(),
                            loss=tf.keras.losses.MeanAbsoluteError,
                            )
-        try:
-            self.model.load_weights(self.model_checkpoint_dir)
-        except(Exception,) as e:
-            print(e)
 
     def train(self, boards, td_values):
         early_stopping_callback = tf.keras.callbacks.EarlyStopping(
             monitor='loss',
             mode='min',
             verbose=1,
-            patience=4)
+            patience=5)
         model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
-            filepath=self.model_checkpoint_dir,
+            filepath=self.model_weights_path,
             save_weights_only=True,
             monitor='loss',
             mode='min',
             save_best_only=True)
-        callbacks_list = [early_stopping_callback, model_checkpoint_callback]
+        callbacks_list = [early_stopping_callback]
         self.model.fit(boards, td_values,
-                       epochs=12,
+                       epochs=20,
                        callbacks=callbacks_list,
                        )
+
+    def save_weights(self):
+        self.model.save_weights(self.model_weights_path)
