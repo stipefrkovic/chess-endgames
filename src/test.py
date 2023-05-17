14f89cfde0eb0c27fd44975bf2058bc0e416a282
stipefrkovic <stipe.frkovic@tutanota.com>
2023-05-17 14:17:56 +0200

    finished new endgame generation

--- src/test.py
+++ src/test.py
@@ -79,108 +79,128 @@ def td_lambda_gradient_test_2():
 #         return board  
 
 
-def get_neighbor_squares(piece):
-    file = piece[0]
-    rank = piece[1]
+class ChessSquare():
     num_files = 8
     num_ranks = 8
-    neighbor_squares = []
-    # Check bottom-left neighbor
-    if rank > 0 and file > 0:
-        neighbor_squares.append((file-1, rank-1))
-    # Check bottom neighbor
-    if rank > 0:
-        neighbor_squares.append((file, rank-1))
-    # Check bottom-right neighbor
-    if rank > 0 and file < num_files - 1:
-        neighbor_squares.append((file+1, rank-1))
-    # Check left neighbor
-    if file > 0:
-        neighbor_squares.append((file-1, rank))
-    # Check right neighbor
-    if file < num_files - 1:
-        neighbor_squares.append((file+1, rank))
-    # Check top-left neighbor
-    if rank < num_ranks - 1 and file > 0:
-        neighbor_squares.append((file-1, rank+1))
-    # Check top neighbor
-    if rank < num_ranks - 1:
-        neighbor_squares.append((file, rank+1))
-    # Check top-right neighbor
-    if rank < num_ranks - 1 and file < num_files - 1:
-        neighbor_squares.append((file+1, rank+1))
-    return neighbor_squares
+
+    def __init__(self, file, rank):
+        self.file = file
+        self.rank = rank
+
+    def __repr__(self):
+        return f"({self.file}, {self.rank})"
+    
+    def __eq__(self, other): 
+        if not isinstance(other, ChessSquare):
+            return NotImplemented
+        return self.file == other.file and self.rank == other.rank
+
+    
+    def create_square(self):
+        return chess.square(self.file, self.rank)
+    
+    def legal_neighbors(self):
+        neighbor_squares = []
+        # Check bottom-left neighbor
+        if self.rank > 0 and self.file > 0:
+            neighbor_squares.append(ChessSquare(self.file-1, self.rank-1))
+        # Check bottom neighbor
+        if self.rank > 0:
+            neighbor_squares.append(ChessSquare(self.file, self.rank-1))
+        # Check bottom-right neighbor
+        if self.rank > 0 and self.file < self.num_files - 1:
+            neighbor_squares.append(ChessSquare(self.file+1, self.rank-1))
+        # Check left neighbor
+        if self.file > 0:
+            neighbor_squares.append(ChessSquare(self.file-1, self.rank))
+        # Check right neighbor
+        if self.file < self.num_files - 1:
+            neighbor_squares.append(ChessSquare(self.file+1, self.rank))
+        # Check top-left neighbor
+        if self.rank < self.num_ranks - 1 and self.file > 0:
+            neighbor_squares.append(ChessSquare(self.file-1, self.rank+1))
+        # Check top neighbor
+        if self.rank < self.num_ranks - 1:
+            neighbor_squares.append(ChessSquare(self.file, self.rank+1))
+        # Check top-right neighbor
+        if self.rank < self.num_ranks - 1 and self.file < self.num_files - 1:
+            neighbor_squares.append(ChessSquare(self.file+1, self.rank+1))
+        return neighbor_squares
+
+class ChessBoard():
+    def __init__(self, squares):
+        self.squares = squares
+
+    def random_square(self):
+        square = random.choice(self.squares)
+        return square
+    
+    def remove_illegal_squares(self, board):
+        for square_idx, square in enumerate(self.squares):
+            if board.piece_at(square.create_square()) is not None:
+                self.squares[square_idx] = None
+            else:
+                board.set_piece_at(square.create_square(), chess.Piece(chess.KING, chess.BLACK))
+                if not board.is_valid():
+                    self.squares[square_idx] = None
+                board.remove_piece_at(square.create_square())
+        # for i in range(8):
+        #     print(self.squares[i*8 : i*8+8])
+        # print('\n')
 
 
-def flood_fill(chess_squares_list, square, region):
-    if square is None or square not in chess_squares_list:
-        return
-    else:
-        region.append(square)
-        square_idx = chess_squares_list.index(square)
-        chess_squares_list[square_idx] = None
-        flood_fill(chess_squares_list, (square[0]-1, square[1]-1), region)
-        flood_fill(chess_squares_list, (square[0]-1, square[1]), region)
-        flood_fill(chess_squares_list, (square[0]-1, square[1]+1), region)
-        flood_fill(chess_squares_list, (square[0], square[1]-1), region)
-        flood_fill(chess_squares_list, (square[0], square[1]+1), region)
-        flood_fill(chess_squares_list, (square[0]+1, square[1]-1), region)
-        flood_fill(chess_squares_list, (square[0]+1, square[1]), region)
-        flood_fill(chess_squares_list, (square[0]+1, square[1]+1), region)
+    def flood_fill(self, square, region):
+        if square is None or square not in self.squares:
+            return
+        else:
+            region.append(square)
+            square_idx = self.squares.index(square)
+            self.squares[square_idx] = None
+            neighbors = square.legal_neighbors()
+            for neighbor in neighbors:
+                self.flood_fill(neighbor, region)
+
+    def find_smallest_region(self):
+        regions = []
+        for square in self.squares:
+            if square is not None:
+                region = []
+                self.flood_fill(square, region)
+                if region:
+                    regions.append(region)
+                    # for i in range(8):
+                    #     print(self.squares[i*8 : i*8+8])
+                    # print('\n')
+        # for region in regions:
+        #     print(region)
+        return min(regions, key=len)
+
 
 
 def cool():
+    chess_squares = ChessBoard([ChessSquare(file, rank) for rank in range(ChessSquare.num_ranks-1, -1, -1) for file in range(0, ChessSquare.num_files, 1)])
     board = chess.Board().empty()
-    
-    num_files = 8
-    num_ranks = 8
-
-    chess_squares_matrix = [[(file, rank) for file in range(0, num_files, 1)] for rank in range(num_ranks-1, -1, -1)]
-    chess_squares_list = list(chain.from_iterable(chess_squares_matrix))
 
     # KING
-    king_square = random.choice(chess_squares_list)
-    board.set_piece_at(chess.square(king_square[0], king_square[1]), chess.Piece(chess.KING, chess.WHITE))
-    print(king_square, "\n", board, "\n")
+    king_square = chess_squares.random_square()
+    board.set_piece_at(king_square.create_square(), chess.Piece(chess.KING, chess.WHITE))
 
     # NEIGHBORS
-    neighbor_squares = get_neighbor_squares(king_square)
-    piece_square = random.choice(neighbor_squares)
-    board.set_piece_at(chess.square(piece_square[0], piece_square[1]), chess.Piece(chess.ROOK, chess.WHITE))
-    print(piece_square, "\n", board, "\n")
+    neighbor_squares = ChessBoard(king_square.legal_neighbors())
+    piece_square = neighbor_squares.random_square()
+    board.set_piece_at(piece_square.create_square(), chess.Piece(chess.ROOK, chess.WHITE))
 
-    # REGIONS
-    for square_idx, square in enumerate(chess_squares_list):
-        if board.piece_at(chess.square(square[0], square[1])) is not None:
-            chess_squares_list[square_idx] = None
-            # print(board, "\n")
-        else:
-            board.set_piece_at(chess.square(square[0], square[1]), chess.Piece(chess.KING, chess.BLACK))
-            if not board.is_valid():
-                chess_squares_list[square_idx] = None
-                # print(board, "\n")
-            board.remove_piece_at(chess.square(square[0], square[1]))
-    # print(chess_squares_list)
-    
-    for i in range(8):
-        print(chess_squares_list[i*8 : i*8+8])
-    print('\n')
+    # REGION
+    chess_squares.remove_illegal_squares(board)
+    other_king_squares = ChessBoard(chess_squares.find_smallest_region())
+    other_king_square = other_king_squares.random_square()
+    board.set_piece_at(other_king_square.create_square(), chess.Piece(chess.KING, chess.BLACK))
 
-    regions = []
-    for square in chess_squares_list:
-        if square is not None:
-            region = []
-            flood_fill(chess_squares_list, square, region)
-            if region:
-                regions.append(region)
-                for i in range(8):
-                    print(chess_squares_list[i*8 : i*8+8])
-                print('\n')
-
+    return board
 
 
 def main():
-    cool()
+    print(cool())
 
 if __name__ == "__main__":
     main()
